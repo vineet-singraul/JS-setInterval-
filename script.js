3770e550f9e83cc6267056f77812ccbab1395c21
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-31 22:47:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let start = () => {
+    let counter = 0;
+    let repetation = setInterval(() => {
+        counter++;
+        console.log(counter);
+
+    }, 1000)
+}
+
+
+sstart = () => {
+        clearInterval(repetation)
+}
+
+
